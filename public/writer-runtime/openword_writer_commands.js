d780ce1d082ada5e4f65e03575a97cf3db4dde5b
zgbrenner <zgbrenner@gmail.com>
2026-08-04 23:57:12 -0700

    feat: add immutable Writer command registry

---
+++ public/writer-runtime/openword_writer_commands.js
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+"use strict";
+
+var OPENWORD_WRITER_COMMANDS = Object.freeze({
+  "format.toggleBold": ".uno:Bold",
+  "format.toggleItalic": ".uno:Italic",
+  "format.toggleUnderline": ".uno:Underline",
+  "history.undo": ".uno:Undo",
+  "history.redo": ".uno:Redo",
+  "paragraph.alignLeft": ".uno:LeftPara",
+  "paragraph.alignCenter": ".uno:CenterPara",
+  "paragraph.alignRight": ".uno:RightPara",
+  "paragraph.alignJustify": ".uno:JustifyPara",
+  "list.toggleBullets": ".uno:DefaultBullet",
+  "list.toggleNumbering": ".uno:DefaultNumbering",
+  "insert.pageBreak": ".uno:InsertPagebreak"
+});
